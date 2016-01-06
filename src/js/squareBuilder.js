a0a534876dd282db3d09f0c6d20fcf52f682d9ea
longmatthewh <longmatthewh@gmail.com>
2016-01-06 12:49:39 -0500

    adding focus and hover

--- src/js/squareBuilder.js
+++ src/js/squareBuilder.js
@@ -1,20 +1,32 @@
 function buildSquares() {
-    const SQUARE_COUNT = 220;
+    const SQUARE_PADDING = 20, SQUARE_COUNT = 200 + SQUARE_PADDING;
     const SPEEDS = 10;
     const SPEED_CLASS_PREFIX = 'speed_';
-    const SQUARE_CLASS = 'square';
+    const SQUARE_CLASS = 'square', SQUARE_CLASS_SELECTOR = '.' + SQUARE_CLASS;
     const SQUARE_ELEMENT = 'div';
 
     for (var i = 0; i < SQUARE_COUNT; i++) {
         buildSquare();
     }
+    assignFocus();
 
     function buildSquare() {
-        var speedClassNum = Math.floor(Math.random() * SPEEDS) + 1;
+        var speedClassNum = randomNumber(SPEEDS) + 1;
         var speedClass = SPEED_CLASS_PREFIX + speedClassNum;
 
         var newSquare = document.createElement(SQUARE_ELEMENT);
         newSquare.className = speedClass + ' ' + SQUARE_CLASS;
+        newSquare.setAttribute('tabindex', '-1');
         document.body.appendChild(newSquare);
+    }
+
+    function assignFocus() {
+        var randomSquareIdx = randomNumber(SQUARE_COUNT - SQUARE_PADDING);
+        var randomSquare = document.querySelectorAll(SQUARE_CLASS_SELECTOR)[randomSquareIdx];
+        randomSquare.focus();
+    }
+
+    function randomNumber(max) {
+        return Math.floor(Math.random() * max);
     }
 }
